d1a08212be726f7220ddc799a5ddb2ec62b10297
Nancy <nancyrosas0@gmail.com>
2021-04-12 20:39:25 -0500

    added 'when'

--- app.js
+++ app.js
@@ -40,6 +40,24 @@ const promptUser = () => {
       }
     },
     {
+      type: 'confirm',
+      name: 'confirmAbout',
+      message: 'Would you like to enter some information about yourself for an "About" section?',
+      default: true
+    },
+    {
+      type: 'input',
+      name: 'about',
+      message: 'Provide some info about yourself',
+      when: ({ confirmAbout }) => {
+        if (confirmAbout) {
+          return true;
+        } else {
+          return false;
+        }
+      }
+    },
+    {
       type: 'input',
       name: 'link',
       message: 'Enter your GitHub Link',
@@ -52,11 +70,6 @@ const promptUser = () => {
         }
       }
     },
-    {
-      type: 'input',
-      name: 'about',
-      message: 'Provide some information about yourself:'     
-    }
   ]);
 };
 
