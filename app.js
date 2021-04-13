d6f1e17717bf875602ee836683c0dc83db2defde
Nancy <nancyrosas0@gmail.com>
2021-04-12 19:17:37 -0500

    added questions

--- app.js
+++ app.js
@@ -1,41 +1,22 @@
-const fs = require('fs');
+const inquirer = require('inquirer');
 
-const generatePage = require('./src/page-template.js');
+// const fs = require('fs');
+// const generatePage = require('./src/page-template');
 
-const profileDataArgs = process.argv.slice(2);
-// const name = profileDataArgs[0];
-// const github = profileDataArgs[1];
-const [name, github] = profileDataArgs;
+// const pageHTML = generatePage(name, github);
 
-//const printProfileData = profileDataArr => {
-//   // This...
-//   for (let i = 0; i < profileDataArr.length; i += 1) {
-//     console.log(profileDataArr[i]);
-//   }
+// fs.writeFile('./index.html', pageHTML, err => {
+//   if (err) throw err;
 
-//   console.log('================');
+//   console.log('Portfolio complete! Check out index.html to see the output!');
+// });
 
-//   // Is the same as this...
-//   profileDataArr.forEach(profileItem => console.log(profileItem));
-// };
-
-// printProfileData(profileDataArgs);
-
-// const generatePage = (userName, githubName) => `Name: ${userName}, Github: ${githubName}`;
-// console.log(generatePage('Jane', 'janehub'));
-
-// const generatePage = (userName, githubName) => {
-//   return `
-//   Name: ${userName}
-//   GitHub: ${githubName}
-//   `; 
-// };
-
-
-
- 
-fs.writeFile('index.html', generatePage(name, github), err => {
-  if (err) throw new Error (err);
-
-  console.log('Portfolio complete! Check out index.html to see the output!');
-});
+inquirer
+.prompt([
+  {
+    type: 'input',
+    name: 'name',
+    message: 'What is your name?'
+  }
+])
+.then(answers => console.log(answers));
