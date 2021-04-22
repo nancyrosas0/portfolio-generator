d98f839b294848e57cb025bb1e470f02f26f6c37
Nancy <nancyrosas0@gmail.com>
2021-04-22 10:36:07 -0500

    refactored code

--- src/page-template.js
+++ src/page-template.js
@@ -62,54 +62,54 @@ const generateProjects = projectsArr => {
   `;
 };
 
-// // get array of all non-featured projects
-// const nonFeaturedProjects = projectsArr.filter(project => {
-//   if (!project.feature) {
-//     return true;
-//   } else {
-//     return false;
-//   }
-// });
+// // // get array of all non-featured projects
+// // const nonFeaturedProjects = projectsArr.filter(project => {
+// //   if (!project.feature) {
+// //     return true;
+// //   } else {
+// //     return false;
+// //   }
+// // });
 
-  const featuredProjectHtmlArr = featuredProjects.map(({ name, description, languages, link }) => {
-    return `
-      <div class="col-12 col-md-6 mb-2 bg-dark text-light p-2 flex-column">
-        <h3 class="portfolio-item-title text-light">${name}</h3>
-        <h5 class="portfolio-languages">
-        Built With:
-        ${languages.join(', ')}
-        </h5>
-        <p>${description}</p>
-        <a href="${link}" class="btn mt-auto"><i class="fab fa-github mr-2"></i>View Project on Github</a>
-      </div>
-    `;
-  });
+//   const featuredProjectHtmlArr = featuredProjects.map(({ name, description, languages, link }) => {
+//     return `
+//       <div class="col-12 col-md-6 mb-2 bg-dark text-light p-2 flex-column">
+//         <h3 class="portfolio-item-title text-light">${name}</h3>
+//         <h5 class="portfolio-languages">
+//         Built With:
+//         ${languages.join(', ')}
+//         </h5>
+//         <p>${description}</p>
+//         <a href="${link}" class="btn mt-auto"><i class="fab fa-github mr-2"></i>View Project on Github</a>
+//       </div>
+//     `;
+//   });
 
-  const nonFeaturedProjectHtmlArr = nonFeaturedProjects.map(({ name, description, languages, link }) => {
-    return `
-      <div class="col-12 col-md-6 mb-2 bg-dark text-light p-3 flex-column">
-        <h3 class="portfolio-item-title text-light">${name}</h3>
-        <h5 class="portfolio-languages">
-        Built With:
-        ${languages.join(', ')}
-        </h5>
-        <p>${description}</p>
-        <a href="${link}" class="btn mt-auto><i class="fab fa-github mr-2></i>View Project on Github</a>
-        </div>
-    `;
-    }
-  );
+//   const nonFeaturedProjectHtmlArr = nonFeaturedProjects.map(({ name, description, languages, link }) => {
+//     return `
+//       <div class="col-12 col-md-6 mb-2 bg-dark text-light p-3 flex-column">
+//         <h3 class="portfolio-item-title text-light">${name}</h3>
+//         <h5 class="portfolio-languages">
+//         Built With:
+//         ${languages.join(', ')}
+//         </h5>
+//         <p>${description}</p>
+//         <a href="${link}" class="btn mt-auto><i class="fab fa-github mr-2></i>View Project on Github</a>
+//         </div>
+//     `;
+//     }
+//   );
   
-  return `
-    <section class="my-3" id="portfolio">
-    <h2 class="text-dark bg-primary p-2 display-inline-block">Work</h2>
-    <div class="flex-row justify-space-between">
-      ${featuredProjectHtmlArr.join('')}
-      ${nonFeaturedProjectHtmlArr.join('')}
-      </div>
-      </section>
-  `;
-};
+//   return `
+//     <section class="my-3" id="portfolio">
+//     <h2 class="text-dark bg-primary p-2 display-inline-block">Work</h2>
+//     <div class="flex-row justify-space-between">
+//       ${featuredProjectHtmlArr.join('')}
+//       ${nonFeaturedProjectHtmlArr.join('')}
+//       </div>
+//       </section>
+//   `;
+// };
 
 module.exports = templateData => {
   // destructure page data by section
